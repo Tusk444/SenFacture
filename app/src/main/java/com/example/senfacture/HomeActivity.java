cd1c5181ee5bbd1c89143b60f8c0ee03015f426f
Aly Ndiaye <ndiayealy99@gmail.com>
2022-04-27 03:07:38 +0000

    session + display factures

--- app/src/main/java/com/example/senfacture/HomeActivity.java
+++ app/src/main/java/com/example/senfacture/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.senfacture;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -21,6 +24,16 @@ public class HomeActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
 
+    // creating constant keys for shared preferences.
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    // key for storing email.
+    public static final String EMAIL_KEY = "email_key";
+
+    // variable for shared preferences.
+    SharedPreferences sharedpreferences;
+    String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +49,9 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -53,6 +69,15 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        // getting data from shared prefs and
+        // storing it in our string variable.
+        email = sharedpreferences.getString(EMAIL_KEY, null);
+
+        // initializing our textview and button.
+        TextView welcomeTV = findViewById(R.id.welcomeEmail);
+        welcomeTV.setText(email);
         return true;
     }
 
--- app/src/main/java/com/example/senfacture/MainActivity.java
+++ app/src/main/java/com/example/senfacture/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.senfacture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
@@ -27,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
     private Button btnConnect, btnSignUp;
     private String password;
     public static String email;
+    // variable for shared preferences.
+    SharedPreferences sharedpreferences;
+
+    // creating constant keys for shared preferences.
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    // key for storing email.
+    public static final String EMAIL_KEY = "email_key";
+
+    // key for storing password.
+    public static final String PASSWORD_KEY = "password_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
         txtPassword = findViewById(R.id.txtPassword);
         btnConnect = findViewById(R.id.btnConnect);
         btnSignUp = findViewById(R.id.btnSignUp);
+
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        // in shared prefs inside het string method
+        // we are passing key value as EMAIL_KEY and
+        // default value is
+        // set to null if not present.
+        email = sharedpreferences.getString(EMAIL_KEY, null);
+        password = sharedpreferences.getString(PASSWORD_KEY, null);
 
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,8 +127,18 @@ public class MainActivity extends AppCompatActivity {
                         });
                     }
                     else {
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                        // below two lines will put values for
+                        // email and password in shared preferences.
+                        editor.putString(EMAIL_KEY, txtEmail.getText().toString());
+                        editor.putString(PASSWORD_KEY, txtPassword.getText().toString());
+
+                        // to save our data with key and value.
+                        editor.apply();
                         Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                         intent.putExtra("EMAIL", email);
+                        startActivity(intent);
                     }
                 }catch (Exception e){
                     e.printStackTrace();
